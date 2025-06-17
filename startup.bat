e3f6fda2e8587c7890b6857f54ed050d132130b1
Bertini <bertin_p@etna-alternance.net>
2025-06-17 12:49:45 +0200

    mtn si une app demandé est deja ouverte elle nouvre pas de nouveau l'app

--- startup.bat
+++ startup.bat
@@ -49,32 +49,67 @@ echo === LANCEMENT ===
 
 for %%C in (!cleaned!) do (
     if "%%C"=="1" (
-        echo → Lancement de A...
-        start "" "!app1!"
+        tasklist /fi "imagename eq !proc1!.exe" | find /i "!proc1!.exe" >nul
+        if errorlevel 1 (
+            echo → Lancement de A...
+            start "" "!app1!"
+        ) else (
+            echo → A déjà en cours.
+        )
     )
     if "%%C"=="2" (
-        echo → Lancement de B...
-        start "" "!app2!"
+        tasklist /fi "imagename eq !proc2!.exe" | find /i "!proc2!.exe" >nul
+        if errorlevel 1 (
+            echo → Lancement de B...
+            start "" "!app2!"
+        ) else (
+            echo → B déjà en cours.
+        )
     )
     if "%%C"=="3" (
-        echo → Lancement de C...
-        start "" "!app3!"
+        tasklist /fi "imagename eq !proc3!.exe" | find /i "!proc3!.exe" >nul
+        if errorlevel 1 (
+            echo → Lancement de C...
+            start "" "!app3!"
+        ) else (
+            echo → C déjà en cours.
+        )
     )
     if "%%C"=="4" (
-        echo → Lancement de D...
-        start "" "!app4!"
+        tasklist /fi "imagename eq !proc4!.exe" | find /i "!proc4!.exe" >nul
+        if errorlevel 1 (
+            echo → Lancement de D...
+            start "" "!app4!"
+        ) else (
+            echo → D déjà en cours.
+        )
     )
     if "%%C"=="5" (
-        echo → Lancement de E...
-        start "" "!app5!"
+        tasklist /fi "imagename eq !proc5!.exe" | find /i "!proc5!.exe" >nul
+        if errorlevel 1 (
+            echo → Lancement de E...
+            start "" "!app5!"
+        ) else (
+            echo → E déjà en cours.
+        )
     )
     if "%%C"=="6" (
-        echo → Lancement de F...
-        start "" "!app6!"
+        tasklist /fi "imagename eq !proc6!.exe" | find /i "!proc6!.exe" >nul
+        if errorlevel 1 (
+            echo → Lancement de F...
+            start "" "!app6!"
+        ) else (
+            echo → F déjà en cours.
+        )
     )
     if "%%C"=="7" (
-        echo → Lancement de G...
-        start "" "!app7!"
+        tasklist /fi "imagename eq !proc7!.exe" | find /i "!proc7!.exe" >nul
+        if errorlevel 1 (
+            echo → Lancement de G...
+            start "" "!app7!"
+        ) else (
+            echo → G déjà en cours.
+        )
     )
 )
 
