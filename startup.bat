de5194b24a9c3166dbf358008cf70e3f0408a63b
Bertini <bertin_p@etna-alternance.net>
2025-11-03 23:10:56 +0100

    correction

--- startup.bat
+++ startup.bat
@@ -5,27 +5,30 @@ color 0A
 
 :MENU
 cls
-echo ######################################
-echo ###       CHOISIS UNE OPTION       ###
-echo ######################################
-echo
-echo ================ Code ================
-echo [1] - VS Code
-echo [2] - VS Code & Docker
-echo [3] - VS Code & Android Studio
-echo
-echo ================ Jeux ================
-echo [4] - Celeste
-echo [5] - Cities Skylines II
-echo [6] - Project64
-echo [7] - Geometry Dash
-echo
-echo =============== Autres ===============
-echo [8] - Opera GX
-echo [0] - Quitter
-echo
-echo ######################################
-echo
+echo ########################################
+echo ####       CHOISIS UNE OPTION       ####
+echo ########################################
+echo #                                      #
+echo ################# Code #################
+echo #                                      #
+echo # [1] - VS Code                        #
+echo # [2] - VS Code et Docker              #
+echo # [3] - VS Code et Android Studio      #
+echo #                                      #
+echo ################# Jeux #################
+echo #                                      #
+echo # [4] - Celeste                        #
+echo # [5] - Cities Skylines II             #
+echo # [6] - Project64                      #
+echo # [7] - Geometry Dash                  #
+echo #                                      #
+echo ################ Autres ################
+echo #                                      #
+echo # [8] - Opera GX                       #
+echo # [0] - Quitter                        #
+echo #                                      #
+echo ########################################
+echo.
 set /p choice=Ton choix : 
 
 if "%choice%"=="1" goto VS_CODE_ET_OPERA_GX
@@ -41,38 +44,38 @@ if "%choice%"=="0" exit
 goto MENU
 
 :VS_CODE_ET_OPERA_GX
-start "" "C:\Users\berti\AppData\Local\Programs\Microsoft VS Code\Code.exe" --disable-updates
+call :runApp "C:\Users\berti\AppData\Local\Programs\Microsoft VS Code\Code.exe" --disable-updates
 call :OperaGX
 goto END
 
 :VS_CODE_ET_DOCKER
-start "" "C:\Users\berti\AppData\Local\Programs\Microsoft VS Code\Code.exe" --disable-updates
-start "" "C:\Program Files\Docker\Docker\Docker Desktop.exe"
+call :runApp "C:\Users\berti\AppData\Local\Programs\Microsoft VS Code\Code.exe" --disable-updates
+call :runApp "C:\Program Files\Docker\Docker\Docker Desktop.exe"
 call :OperaGX
 goto END
 
 :VS_CODE_ET_ANDROID_STUDIO
-start "" "C:\Users\berti\AppData\Local\Programs\Microsoft VS Code\Code.exe" --disable-updates
-start "" "C:\Program Files\Docker\Docker\Docker Desktop.exe"
-start "" "C:\Program Files\Android\Android Studio\bin\studio64.exe"
+call :runApp "C:\Users\berti\AppData\Local\Programs\Microsoft VS Code\Code.exe" --disable-updates
+call :runApp "C:\Program Files\Docker\Docker\Docker Desktop.exe"
+call :runApp "C:\Program Files\Android\Android Studio\bin\studio64.exe"
 call :OperaGX
 goto END
 
 :CELESTE
-start "" "C:\Users\berti\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Steam\Celeste.url"
+call :runApp "C:\Users\berti\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Steam\Celeste.url"
 goto END
 
 :CITIES_SKYLINES_II
-start "" "C:\Users\berti\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Steam\Cities Skylines II.url"
+call :runApp "C:\Users\berti\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Steam\Cities Skylines II.url"
 goto END
 
 :PROJECT_64
-start "" "C:\Program Files (x86)\Project64 3.0\Project64.exe"
+call :runApp "C:\Program Files (x86)\Project64 3.0\Project64.exe"
 call :OperaGX
 goto END
 
 :GEOMETRY_DASH
-start "" "C:\Users\berti\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Steam\Geometry Dash.url"
+call :runApp "C:\Users\berti\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Steam\Geometry Dash.url"
 goto END
 
 :OPERA_GX
@@ -82,7 +85,23 @@ goto END
 :OperaGX
 tasklist /FI "IMAGENAME eq opera.exe" | find /I "opera.exe" >nul
 if errorlevel 1 (
-    start "" "C:\Users\berti\AppData\Local\Programs\Opera GX\opera.exe"
+    call :runApp "C:\Users\berti\AppData\Local\Programs\Opera GX\opera.exe"
+)
+exit /b
+
+:runApp
+rem Usage: call :runApp "C:\chemin\vers\app.exe" [args...]
+set "APP=%~1"
+shift
+set "ARGS=%*"
+if exist "%APP%" (
+    if "%ARGS%"=="" (
+        start "" "%APP%"
+    ) else (
+        start "" "%APP%" %ARGS%
+    )
+) else (
+    echo Fichier non trouve: %APP%
 )
 exit /b
 
