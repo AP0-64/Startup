7eb5b30aa95fdd942e81bdf1ba85420e874648fc
Bertini <bertin_p@etna-alternance.net>
2025-11-03 22:54:05 +0100

    mise à jour

--- startup.bat
+++ startup.bat
@@ -5,27 +5,37 @@ color 0A
 
 :MENU
 cls
-echo ======================================
-echo           CHOISIS UNE OPTION
-echo ======================================
+echo ######################################
+echo ###       CHOISIS UNE OPTION       ###
+echo ######################################
+echo
+echo ================ Code ================
 echo [1] - VS Code
-echo [2] - Blogtor
-echo [3] - Opera GX
+echo [2] - VS Code & Docker
+echo [3] - VS Code & Android Studio
+echo
+echo ================ Jeux ================
 echo [4] - Celeste
 echo [5] - Cities Skylines II
 echo [6] - Project64
 echo [7] - Geometry Dash
+echo
+echo =============== Autres ===============
+echo [8] - Opera GX
 echo [0] - Quitter
-echo ======================================
+echo
+echo ######################################
+echo
 set /p choice=Ton choix : 
 
 if "%choice%"=="1" goto VS_CODE_ET_OPERA_GX
-if "%choice%"=="2" goto BLOGTOR
-if "%choice%"=="3" goto OPERA_GX
+if "%choice%"=="2" goto VS_CODE_ET_DOCKER
+if "%choice%"=="3" goto VS_CODE_ET_ANDROID_STUDIO
 if "%choice%"=="4" goto CELESTE
 if "%choice%"=="5" goto CITIES_SKYLINES_II
 if "%choice%"=="6" goto PROJECT_64
 if "%choice%"=="7" goto GEOMETRY_DASH
+if "%choice%"=="8" goto OPERA_GX
 if "%choice%"=="0" exit
 
 goto MENU
@@ -35,13 +45,16 @@ start "" "C:\Users\berti\AppData\Local\Programs\Microsoft VS Code\Code.exe" --di
 call :OperaGX
 goto END
 
-:BLOGTOR
+:VS_CODE_ET_DOCKER
 start "" "C:\Users\berti\AppData\Local\Programs\Microsoft VS Code\Code.exe" --disable-updates
-start "" "C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\IDE\devenv.exe"
+start "" "C:\Program Files\Docker\Docker\Docker Desktop.exe"
 call :OperaGX
 goto END
 
-:OPERA_GX
+:VS_CODE_ET_ANDROID_STUDIO
+start "" "C:\Users\berti\AppData\Local\Programs\Microsoft VS Code\Code.exe" --disable-updates
+start "" "C:\Program Files\Docker\Docker\Docker Desktop.exe"
+start "" "C:\Program Files\Android\Android Studio\bin\studio64.exe"
 call :OperaGX
 goto END
 
@@ -60,6 +73,10 @@ goto END
 
 :GEOMETRY_DASH
 start "" "C:\Users\berti\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Steam\Geometry Dash.url"
+goto END
+
+:OPERA_GX
+call :OperaGX
 goto END
 
 :OperaGX
